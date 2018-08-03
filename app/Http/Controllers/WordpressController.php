e0c71bfae330eb205b6546eacae22b4efa3d0605
purplenimbus <you@example.com>
2018-08-03 19:38:54 +0100

    ProcessUpload Job
    Processing Jobs in Controller due to serialization issues

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -11,9 +11,58 @@ use Illuminate\Http\Request;
 use App\Upload;
 use App\Jobs\ProcessUpload;
 
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+
 class WordpressController extends Controller
 {
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+	
+	var $http;
+	var $wordpress_url;
+	var $guzzle;
+	
+	function __construct(){
+		$this->guzzle = new GuzzleClient();
+		$this->wordpress_url = env('NIMBUS_MEDIA_API_ENPOINT');
+	}
+	
+	protected function  WP($request_type,$endpoint,$payload){
+		$url = $this->wordpress_url.$endpoint;
+		
+		$stack = $this->handler(env('NIMBUS_MEDIA_CLIENT_KEY'),env('NIMBUS_MEDIA_CLIENT_SECRET'),env('NIMBUS_MEDIA_OAUTH_TOKEN_SECRET'),env('NIMBUS_MEDIA_OAUTH_TOKEN'));
+
+		$options = array( 	
+							'form_params' => $payload ? $payload : null , 
+							'handler' => $stack, 
+							'auth' => 'oauth',
+							'exceptions ' =>  false,
+							'query' => [	'per_page' => 100	 ]
+		);
+		
+		return $this->guzzle->request($request_type,$url,$options);
+	}
+	
+	private function handler($consumer_key,$consumer_secret,$token_secret,$token){
+		$handler = new CurlHandler();
+		
+		$stack = HandlerStack::create($handler);
+
+		$middleware = new Oauth1([
+			'consumer_key'    => $consumer_key,
+			'consumer_secret' => $consumer_secret,
+			'token_secret'    => $token_secret,
+			'token'           => $token,
+			'request_method' => Oauth1::REQUEST_METHOD_QUERY,
+			'signature_method' => Oauth1::SIGNATURE_METHOD_HMAC
+		]);
+		
+		$stack->push($middleware);
+		
+		return $stack;
+	}
 	
 	public function processFile(Request $request){
 		try{
@@ -23,15 +72,18 @@ class WordpressController extends Controller
 			
 			$upload->save();
 			
-			$files = array_map(function($v){
-				return [
-					'path' => $v->path()
-				];
-			},$request->file('files'));
-		
-			ProcessUpload::dispatch($files,$upload);
+			$file = $request->file('files')[0];
 			
-			return response()->json(['uuid' => $upload->uuid,'status' => 'processing'],200);
+			$payload = [
+				'title' => $file->getClientOriginalName(),
+				'files' => $file
+			];
+			
+			$response = $this->WP('POST','/media',$payload);
+			
+			var_dump($response);
+					
+			//return response()->json(['uuid' => $upload->uuid,'status' => 'processing'],200);
 			
 		}catch(Exception $e){
 			//var_dump($e);
--- app/Jobs/ProcessUpload.php
+++ app/Jobs/ProcessUpload.php
@@ -8,61 +8,10 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Handler\CurlHandler;
-use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 class ProcessUpload implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-	
-	var $http;
-	var $wordpress_url;
-	var $upload;
-	var $files;
-	
-	function __construct($files,$upload){
-		$this->guzzle = new GuzzleClient();
-		$this->wordpress_url = env('NIMBUS_MEDIA_API_ENPOINT');
-		$this->upload = $upload;
-		$this->files = $files;
-	}
-	
-	protected function  WP($type,$endpoint,$payload){
-		$url = $this->wordpress_url.$endpoint;
-		
-		$stack = $this->handler(env('NIMBUS_MEDIA_CLIENT_KEY'),env('NIMBUS_MEDIA_CLIENT_SECRET'),env('NIMBUS_MEDIA_OAUTH_TOKEN_SECRET'),env('NIMBUS_MEDIA_OAUTH_TOKEN'));
-
-		$options = array( 	
-							'form_params' => $payload ? $payload : null , 
-							'handler' => $stack, 
-							'auth' => 'oauth',
-							'exceptions ' =>  false,
-							'query' => [	'per_page' => 100	 ]
-		);
-		
-		return $response = $this->guzzle->request($type,$url,$options);
-	}
-	
-	private function handler($consumer_key,$consumer_secret,$token_secret,$token){
-		$handler = new CurlHandler();
-		
-		$stack = HandlerStack::create($handler);
-
-		$middleware = new Oauth1([
-			'consumer_key'    => $consumer_key,
-			'consumer_secret' => $consumer_secret,
-			'token_secret'    => $token_secret,
-			'token'           => $token,
-			'request_method' => Oauth1::REQUEST_METHOD_QUERY,
-			'signature_method' => Oauth1::SIGNATURE_METHOD_HMAC
-		]);
-		
-		$stack->push($middleware);
-		
-		return $stack;
-	}
 
     /**
      * Execute the job.
@@ -71,6 +20,6 @@ class ProcessUpload implements ShouldQueue
      */
     public function handle()
     {
-        var_dump($this->files);
+
     }
 }
