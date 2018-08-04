cb584f361ed519f240514436c0eb13fa07537f61
purplenimbus <you@example.com>
2018-08-04 14:14:19 -0700

    handle file processing in the NimbusWP Class

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -26,10 +26,11 @@ class WordpressController extends Controller
 	var $http;
 	var $wordpress_url;
 	var $guzzle;
+	var $api;
 	
 	function __construct(){
 		$this->guzzle = new GuzzleClient();
-		$this->NIMBUS = new NimbusWP(env('NIMBUS_MEDIA_API_ENPOINT'));
+		$this->api = new NimbusWP(env('NIMBUS_MEDIA_API_ENPOINT'));
 	}
 	
 	
@@ -38,64 +39,11 @@ class WordpressController extends Controller
 			//validate file?
 			
 			$upload = new Upload;
-						
-			$file = $request->file('files')[0];
 			
-			$payload = [
-				'title' => $file->getClientOriginalName(),
-				'files' => $file
-			];
-			
-			if(Auth::user()->uuid){
-				$payload['meta'] = [
-					'user_uuid' => Auth::user()->uuid
-				];	
-			}	
-			
-			$options = [
-				'headers'	=>	[	
-					'Content-Disposition' => 'attachment; filename='.$file->getClientOriginalName(),
-					//'Content-type' => $file->getClientMimeType()
-				],
-				'form_params' => $payload
-				//'body' => file_get_contents($file->path())
-			];
-			
-			//var_dump($file->getClientMimeType());
-			
-			$response = $this->NIMBUS->API('POST','media',$options);
-						
-			if($response->getStatusCode() === 200 || $response->getStatusCode() === 201){
-				
-				$upload->processed = true;
-				
-				$payload = json_decode($response->getBody()->getContents());
-							
-				$upload->url = $payload->guid->rendered;
-				
-				$upload->meta = [
-					'wpId' => $payload->id
-				];
-				
-				if(Auth::user()->uuid){
-					$upload->user_uuid = Auth::user()->uuid; //move to middelware or model?
-				}
-				
-				$upload->save();
-				
-				if(Auth::user()->uuid){
-					Auth::user()->notify(new UploadProcessed($upload));
-				}
-							
-				$data = ['uuid' => $upload->uuid,'status' => 'processed','url' => $upload->url];
-				
-				 \Log::info('Processed '.$file->getClientOriginalName());
-				
-			}else{
-				$data = ['message' => 'somethings wrong', 'errors' => $response->getReasonPhrase()];
-			}
+			$res = $this->api->process($request,$upload);
 
-			return response()->json($data,$response->getStatusCode());
+			return response()->json($res,$res['status']);
+			
 		}catch(Exception $e){
 			return response()->json(['status' => $e->getMessage() ],500);	
 		}
