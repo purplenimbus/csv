694a12e1bb9c30025001374c3031c514d9931ef4
purplenimbus <you@example.com>
2018-08-10 18:23:39 -0700

    stage userpillcomponent

---
+++ resources/assets/js/components/UserPillComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+		<a v-on:click="open">{{ user }}</a>
+		<modal-component v-on:modal="init"></modal-component>
+	</div>
+</template>
+
+<script>
+    export default {
+		data:function(){
+			return {
+				user:false,
+				loading:true
+			}
+		},
+		computed : {
+
+		},
+		methods : {
+			open(){
+				var body = '';
+				console.log('UserPill modal open');
+				this.$broadcast('modal',body);
+			},
+			init(){
+				var self = this;
+				console.log('UserPill component init' ,self);	
+				self.loading = false;
+			}
+		},
+        mounted() {
+            console.log('UserPill Component mounted.');
+			this.init();
+        }
+    }
+</script>
